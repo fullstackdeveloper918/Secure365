82a4d8fd16457e58827d967cb5195c3876a2fb6e
fullstack developer <fullstackdeveloper918@gmail.com>
2024-12-11 10:41:10 +0530

    fix hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -59,7 +59,8 @@ const Hero = () => {
   }
 
   return (
-    <section className='relative overlay banner_robot'>
+    <>
+   
       <Image src="/Images/homepage_banner.png" className='absolute' layout='fill' alt='background image' />
       <div className='relative container mx-auto grid grid-cols-1 gap-6 lg:gap-0 md:grid-cols-2 items-center justify-between  lg:px-8 pt-24 sm:pt-24 pb-0 min_height'>
         <div className='flex flex-col gap-y-4 justify-center items-center text-center sm:items-start sm:text-start pb-10'>
@@ -67,7 +68,7 @@ const Hero = () => {
            variants={FadeUp(0.6)}
            initial="initial"
            animate="animate"
-          className='banner_heading font-Axiforma'>
+           className='banner_heading font-Axiforma'>
             <span className='banner_heading_medium'>{data?.pages?.banner_heading}</span>
             <span className='mr-3'> {data?.pages?.banner_heading_second}</span>
             <span className='text_blue'>{data?.pages?.banner_heading_thired}</span>
@@ -76,14 +77,14 @@ const Hero = () => {
            variants={FadeUp(0.8)}
            initial="initial"
            animate="animate"
-          className='max-w-md sm:max-w-[39rem] leading-normal text-muted-foreground text-sm sm:text-xl text-white sm:leading-8'>
+           className='max-w-md sm:max-w-[39rem] leading-normal text-muted-foreground text-sm sm:text-xl text-white sm:leading-8'>
             {data?.pages?.banner_sub_headline}
           </motion.h2>
           <motion.div
            variants={FadeUp(1)}
            initial="initial"
            animate="animate"
-          className='flex flex-wrap items-center text-white gap-4 mt-5'>
+           className='flex flex-wrap items-center text-white gap-4 mt-5'>
             <a href='/products' className='btn_one global_btn capitalize'>
               {data?.pages?.get_started}
             </a>
@@ -94,22 +95,23 @@ const Hero = () => {
         </div>
         <div
               
-        className='flex items-center justify-center md:justify-end'>
+              className='flex items-center justify-center md:justify-end'>
           <motion.img
            initial={{y:-100, opacity: 0}}
            animate={{y:0, opacity: 1}}
            transition={{duration: 0.9, delay: 0.4, ease: "easeInOut"}}
-            priority
-            src={data?.pages?.banner_image?.url}
-            className='banner_absolute'
-            alt='Hero image'
-            width={500}
-            height={500}
-            objectFit='cover'
-          />
+           priority
+           src={data?.pages?.banner_image?.url}
+           className='banner_absolute'
+           alt='Hero image'
+           width={500}
+           height={500}
+           objectFit='cover'
+           />
         </div>
       </div>
-    </section>
+
+           </>
   );
 };
 
