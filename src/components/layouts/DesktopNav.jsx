99c3fc984abcfb3ebb06983d8e469ae9057cbbb8
fullstack developer <fullstackdeveloper918@gmail.com>
2024-12-20 18:39:09 +0530

    kjbvd

--- src/components/layouts/DesktopNav.jsx
+++ src/components/layouts/DesktopNav.jsx
@@ -4,13 +4,12 @@ import Secure from "../../../public/svg/secure.svg";
 import React, { useEffect, useState } from "react";
 import {
   NavigationMenu,
-  NavigationMenuLink,
   NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "../ui/navigation-menu";
-import  ListItem  from "./ListItem";
+import ListItem from "./ListItem";
 
 const DesktopNav = () => {
   const [data, setData] = useState([]);
@@ -23,51 +22,50 @@ const DesktopNav = () => {
 
         const result = await response.json();
         setData(result?.menu_items);
-      } catch (error) {
-      }
+      } catch (error) {}
     };
 
     fetchData();
   }, []);
 
-
   return (
     <div className="hidden lg:flex items-center w-full md:justify-between w-100 ">
-     
       <Link href="/" className="flex space-x-2">
-       
-        <Image src='/Images/secure_black.png' width={240} height={80} alt="Secure Logo" />
+        <Image
+          src="/Images/secure_black.png"
+          width={240}
+          height={80}
+          alt="Secure Logo"
+        />
       </Link>
-     
+
       <NavigationMenu>
         <NavigationMenuList>
           {data?.map((item, index) => (
-            
-            
-              <Link href={`/${item?.slug}`} key={index}>
-           
-              <NavigationMenuItem >
-                <NavigationMenuTrigger className='font-Axiforma' chever={item?.children?.length> 0}>{item?.title}</NavigationMenuTrigger>
-                
-                {
-                  item?.children?.length > 0 &&
-               
-                <NavigationMenuContent>
-                  <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr] ">
-                    {  
-                      item?.children?.map((child, childindex) => (
+            <Link href={`/${item?.slug}`} key={index}>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger
+                  className="font-Axiforma"
+                  chever={item?.children?.length > 0}
+                >
+                  {item?.title}
+                </NavigationMenuTrigger>
+
+                {item?.children?.length > 0 && (
+                  <NavigationMenuContent>
+                    <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr] ">
+                      {item?.children?.map((child, childindex) => (
                         <React.Fragment key={child?.title}>
-                        <ListItem  href={`/contact-us`} title={child?.title}>
-                        Explore {child?.title}
-                      </ListItem>
+                          <ListItem href={`/contact-us`} title={child?.title}>
+                            Explore {child?.title}
+                          </ListItem>
                         </React.Fragment>
-                      ))
-                    }
-                  </ul>
-                </NavigationMenuContent>
-                 }
+                      ))}
+                    </ul>
+                  </NavigationMenuContent>
+                )}
               </NavigationMenuItem>
-               </Link>
+            </Link>
           ))}
         </NavigationMenuList>
       </NavigationMenu>
